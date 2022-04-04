c58264272e41c398cfc979cc327fabd07c8147b9
yongCloude <djkye0001@naver.com>
2022-04-04 13:26:06 +0900

    [FEAT] 단일 도서관 검색 mock api 추가

--- src/main/java/com/cloudlibrary/library/ui/controller/LibraryController.java
+++ src/main/java/com/cloudlibrary/library/ui/controller/LibraryController.java
@@ -1,6 +1,7 @@
 package com.cloudlibrary.library.ui.controller;
 
 
+import com.cloudlibrary.library.application.service.LibraryOperationUseCase;
 import com.cloudlibrary.library.application.service.LibraryReadUseCase;
 import com.cloudlibrary.library.ui.view.library.LibraryView;
 import io.swagger.annotations.Api;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 
 @Slf4j
 @RestController
@@ -20,19 +23,39 @@ import org.springframework.web.bind.annotation.RestController;
 public class LibraryController {
 
     private final LibraryReadUseCase libraryReadUseCase;
+    private final LibraryOperationUseCase libraryOperationUseCase;
 
     @Autowired
-    public LibraryController(LibraryReadUseCase libraryReadUseCase) {
+    public LibraryController(LibraryReadUseCase libraryReadUseCase, LibraryOperationUseCase libraryOperationUseCase) {
         this.libraryReadUseCase = libraryReadUseCase;
+        this.libraryOperationUseCase = libraryOperationUseCase;
     }
     //TODO 도서관 등록
 
     //TODO 도서관 LIST 조회
+    @GetMapping("")
+    public ResponseEntity<List<LibraryView>> getLibraries(){
+
+        return null;
+    }
 
     //TODO 특정 도서관 조회
     @GetMapping("/{id}")
     public ResponseEntity<LibraryView> getLibraries(@PathVariable("id") long id){
-        return null;
+
+        // id를 갖는 임의 도서관 도메인 생성
+        var command = LibraryOperationUseCase.LibraryCreatedCommand.builder()
+                .id(id)
+                .name(id + " 테스트 생성된 도서관")
+                .address("서울시 강서구 공항동 444-3")
+                .email("test@google.com")
+                .tel("02-1111-2222")
+                .holiday("월")
+                .build();
+
+        var result = libraryOperationUseCase.createLibrary(command);
+        return ResponseEntity.ok(new LibraryView(result));
+
     }
 
     //TODO 도서관 삭제
