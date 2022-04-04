d068e3d9e06c91c94cd82fd5cef774a08acd8259
yongCloude <djkye0001@naver.com>
2022-04-04 13:54:59 +0900

    [TEST] 단일 도서관 조회 테스트 코드 추가

--- src/test/java/com/cloudlibrary/library/infrastructure/persistance/Memory/repository/MemoryEntityRepositoryTest.java
+++ src/test/java/com/cloudlibrary/library/infrastructure/persistance/Memory/repository/MemoryEntityRepositoryTest.java
@@ -4,7 +4,10 @@ import com.cloudlibrary.library.application.domain.Library;
 import com.cloudlibrary.library.application.service.LibraryOperationUseCase;
 import com.cloudlibrary.library.infrastructure.persistance.Memory.Entity.LibraryEntity;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
 
 import java.util.Optional;
 
@@ -16,12 +19,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemoryEntityRepositoryTest {
     LibraryEntityRepository memoryEntityRepository = new MemoryEntityRepository();
 
+
     @Test
     public void createTest() {
         //given
         // id를 갖는 임의 도서관 도메인 생성
         var command = LibraryOperationUseCase.LibraryCreatedCommand.builder()
-                .id(10)
+                .id(11)
                 .name(10 + " 테스트 생성된 도서관")
                 .address("서울시 강서구 공항동 444-3")
                 .email("test@google.com")
@@ -32,8 +36,27 @@ class MemoryEntityRepositoryTest {
         LibraryEntity result = memoryEntityRepository.create(command);
 
         //then
-        assertThat(result.getId()).isEqualTo(10);
+        assertThat(result.getId()).isEqualTo(11);
 
+    }
+
+    @Test
+    public void findLibraryByIdTest(){
+
+        // id를 갖는 임의 도서관 도메인 생성
+        var command = LibraryOperationUseCase.LibraryCreatedCommand.builder()
+                .id(13)
+                .name(13 + " 테스트 생성된 도서관")
+                .address("서울시 강서구 공항동 444-3")
+                .email("test@google.com")
+                .tel("02-1111-2222")
+                .holiday("월")
+                .build();
+        //when
+        LibraryEntity result = memoryEntityRepository.create(command);
+
+        Optional<Library> find = memoryEntityRepository.findLIbraryById(13);
+        assertThat(find.get().toString()).isEqualTo(result.toLibrary().toString());
 
     }
 
