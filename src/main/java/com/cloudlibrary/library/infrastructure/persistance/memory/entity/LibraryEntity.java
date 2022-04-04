175e4ff3ea77ddc1c51b89ceecfff26c44326195
yongCloude <djkye0001@naver.com>
2022-04-05 02:01:24 +0900

    [fix] 누락된 library info 및 library rules 필드 추가 및 생성자, toLibrary 수정

--- src/main/java/com/cloudlibrary/library/infrastructure/persistance/memory/entity/LibraryEntity.java
+++ src/main/java/com/cloudlibrary/library/infrastructure/persistance/memory/entity/LibraryEntity.java
@@ -8,18 +8,32 @@ import lombok.Setter;
 import lombok.ToString;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @ToString
 public class LibraryEntity implements Serializable {
+    // library Info
     private long id;
     private String name;
     private String address;
     private String email;
     private String tel;
     private String holiday;
+    private String operatingTime;
+    private String loanAvailability;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+
+
+    // library rules
+    private int lendingAvailableCount;
+    private int lendingAvailableDays;
+    private int overdueCount;
+    private int longtermOverdueDays;
+    private int lendingLimitDays;
 
     public LibraryEntity(Library library) {
         this.id = library.getId();
@@ -27,7 +41,17 @@ public class LibraryEntity implements Serializable {
         this.address = library.getAddress();
         this.email = library.getEmail();
         this.tel = library.getTel();
-        this.holiday = getHoliday();
+        this.holiday = library.getHoliday();
+        this.operatingTime = library.getOperatingTime();
+        this.loanAvailability = library.getLoanAvailability();
+        this.createdAt = library.getCreatedAt();
+        this.updatedAt = library.getUpdatedAt();
+
+        this.lendingAvailableCount = library.getLendingAvailableCount();
+        this.lendingAvailableDays = library.getLendingAvailableDays();
+        this.overdueCount = library.getOverdueCount();
+        this.longtermOverdueDays = library.getLongtermOverdueDays();
+        this.lendingLimitDays = library.getLendingLimitDays();
     }
 
     public Library toLibrary(){
@@ -38,6 +62,15 @@ public class LibraryEntity implements Serializable {
                 .email(this.email)
                 .tel(this.tel)
                 .holiday(this.holiday)
+                .operatingTime(this.operatingTime)
+                .loanAvailability(this.loanAvailability)
+                .createdAt(this.createdAt)
+                .updatedAt(this.updatedAt)
+                .lendingAvailableCount(this.lendingAvailableCount)
+                .lendingAvailableDays(this.lendingAvailableDays)
+                .overdueCount(this.overdueCount)
+                .longtermOverdueDays(this.longtermOverdueDays)
+                .lendingLimitDays(this.lendingLimitDays)
                 .build();
     }
 }
