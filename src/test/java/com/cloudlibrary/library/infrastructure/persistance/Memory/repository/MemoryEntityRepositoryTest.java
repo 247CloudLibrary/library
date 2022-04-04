80825a7ae876883e09b7825d6432cd73c983a0d6
yongCloude <djkye0001@naver.com>
2022-04-04 14:36:39 +0900

    [TEST] 도서관 전체 조회 테스트 코드 추가

--- src/test/java/com/cloudlibrary/library/infrastructure/persistance/Memory/repository/MemoryEntityRepositoryTest.java
+++ src/test/java/com/cloudlibrary/library/infrastructure/persistance/Memory/repository/MemoryEntityRepositoryTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.test.context.event.annotation.BeforeTestClass;
 import org.springframework.test.context.event.annotation.BeforeTestMethod;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -60,5 +61,36 @@ class MemoryEntityRepositoryTest {
 
     }
 
+    @Test
+    public void getLibraryListAll(){
+
+        // given
+        var command = LibraryOperationUseCase.LibraryCreatedCommand.builder()
+                .id(13)
+                .name(13 + " 테스트 생성된 도서관")
+                .address("서울시 강서구 공항동 444-3")
+                .email("test@google.com")
+                .tel("02-1111-2222")
+                .holiday("월")
+                .build();
+        //when
+        LibraryEntity result = memoryEntityRepository.create(command);
+        var command2 = LibraryOperationUseCase.LibraryCreatedCommand.builder()
+                .id(15)
+                .name(15 + " 테스트 생성된 도서관")
+                .address("서울시 강서구 공항동 444-3")
+                .email("test@google.com")
+                .tel("02-1111-2222")
+                .holiday("월")
+                .build();
+        //when
+        LibraryEntity result2 = memoryEntityRepository.create(command2);
+        // when
+        List<Library> libraries = memoryEntityRepository.findLibraryAll();
+
+
+        // then
+        System.out.println("libraries = " + libraries);
+    }
 
 }
