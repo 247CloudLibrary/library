7924f75eaf87e7e67d263ef70593cfe1f2589dfa
yongCloude <djkye0001@naver.com>
2022-04-05 00:46:38 +0900

    [fix] 일부 누락된 library info 및 library rules 필드 추가

--- src/main/java/com/cloudlibrary/library/application/domain/Library.java
+++ src/main/java/com/cloudlibrary/library/application/domain/Library.java
@@ -5,16 +5,34 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.sql.Timestamp;
+
 @Getter
 @ToString
 @Builder
 public class Library {
 
+    // library Info
     private final long id;
     private final String name;
     private final String address;
     private final String email;
     private final String tel;
     private final String holiday;
+    private final String operatingTime;
+    private final String loanAvailability;
+    private final Timestamp createdAt;
+    private final Timestamp updatedAt;
+
+
+    // library rules
+    private final int lendingAvailableCount;
+    private final int lendingAvailableDays;
+    private final int overdueCount;
+    private final int longtermOverdueDays;
+    private final int lendingLimitDays;
+
+
+
 
 }
