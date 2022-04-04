15ff643a2d11df63e3224b02f0ba3edca4ac11c5
yongCloude <djkye0001@naver.com>
2022-04-04 12:34:23 +0900

    [FEAT] 로컬 스토리지에서 테스트할 Library Entity 추가

---
+++ src/main/java/com/cloudlibrary/library/infrastructure/persistance/Memory/Entity/LibraryEntity.java
@@ -0,0 +1,43 @@
+package com.cloudlibrary.library.infrastructure.persistance.Memory.Entity;
+
+
+import com.cloudlibrary.library.application.domain.Library;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class LibraryEntity implements Serializable {
+    private long id;
+    private String name;
+    private String address;
+    private String email;
+    private String tel;
+    private String holiday;
+
+    public LibraryEntity(long id, String name, String address, String email, String tel, String holiday) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.tel = tel;
+        this.holiday = holiday;
+    }
+
+    public Library toLibrary(){
+        return Library.builder()
+                .id(this.id)
+                .name(this.name)
+                .address(this.address)
+                .email(this.email)
+                .tel(this.tel)
+                .holiday(this.holiday)
+                .build();
+    }
+}
