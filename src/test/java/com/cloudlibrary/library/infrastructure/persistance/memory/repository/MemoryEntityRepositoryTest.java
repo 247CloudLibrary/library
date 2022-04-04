a70c9ed7d6586d834fb1724c231034ebcb7aa946
yongCloude <djkye0001@naver.com>
2022-04-05 01:58:46 +0900

    [Test] 필드 추가로 인한 테스트 과정 수정

--- src/test/java/com/cloudlibrary/library/infrastructure/persistance/memory/repository/MemoryEntityRepositoryTest.java
+++ src/test/java/com/cloudlibrary/library/infrastructure/persistance/memory/repository/MemoryEntityRepositoryTest.java
@@ -5,6 +5,8 @@ import com.cloudlibrary.library.application.service.LibraryOperationUseCase;
 import com.cloudlibrary.library.infrastructure.persistance.memory.entity.LibraryEntity;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,6 +20,8 @@ class MemoryEntityRepositoryTest {
     @Test
     public void createTest() {
         //given
+        LocalDateTime localDateTime = LocalDateTime.now();
+        Timestamp currentTime = Timestamp.valueOf(localDateTime);
         // id를 갖는 임의 도서관 도메인 생성
         var command = LibraryOperationUseCase.LibraryCreatedCommand.builder()
                 .id(11)
@@ -26,18 +30,29 @@ class MemoryEntityRepositoryTest {
                 .email("test@google.com")
                 .tel("02-1111-2222")
                 .holiday("월")
+                .operatingTime("09:00 ~ 17:00")
+                .loanAvailability("가능")
+                .createdAt(currentTime)
+                .updatedAt(currentTime)
+                .lendingAvailableCount(4)
+                .lendingAvailableDays(3)
+                .overdueCount(2)
+                .lendingLimitDays(5)
                 .build();
         //when
         LibraryEntity result = memoryEntityRepository.create(command);
 
         //then
         assertThat(result.getId()).isEqualTo(11);
+        System.out.println("result = " + result);
 
     }
 
     @Test
     public void findLibraryByIdTest(){
-
+        //given
+        LocalDateTime localDateTime = LocalDateTime.now();
+        Timestamp currentTime = Timestamp.valueOf(localDateTime);
         // id를 갖는 임의 도서관 도메인 생성
         var command = LibraryOperationUseCase.LibraryCreatedCommand.builder()
                 .id(13)
@@ -46,6 +61,14 @@ class MemoryEntityRepositoryTest {
                 .email("test@google.com")
                 .tel("02-1111-2222")
                 .holiday("월")
+                .operatingTime("09:00 ~ 17:00")
+                .loanAvailability("가능")
+                .createdAt(currentTime)
+                .updatedAt(currentTime)
+                .lendingAvailableCount(4)
+                .lendingAvailableDays(3)
+                .overdueCount(2)
+                .lendingLimitDays(5)
                 .build();
         //when
         LibraryEntity result = memoryEntityRepository.create(command);
@@ -59,6 +82,9 @@ class MemoryEntityRepositoryTest {
     public void getLibraryListAll(){
 
         // given
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        Timestamp currentTime = Timestamp.valueOf(localDateTime);
         var command = LibraryOperationUseCase.LibraryCreatedCommand.builder()
                 .id(13)
                 .name(13 + " 테스트 생성된 도서관")
@@ -66,6 +92,14 @@ class MemoryEntityRepositoryTest {
                 .email("test@google.com")
                 .tel("02-1111-2222")
                 .holiday("월")
+                .operatingTime("09:00 ~ 16:00")
+                .loanAvailability("가능")
+                .createdAt(currentTime)
+                .updatedAt(currentTime)
+                .lendingAvailableCount(4)
+                .lendingAvailableDays(3)
+                .overdueCount(2)
+                .lendingLimitDays(5)
                 .build();
         //when
         LibraryEntity result = memoryEntityRepository.create(command);
@@ -75,7 +109,15 @@ class MemoryEntityRepositoryTest {
                 .address("서울시 강서구 공항동 444-3")
                 .email("test@google.com")
                 .tel("02-1111-2222")
-                .holiday("월")
+                .holiday("화")
+                .operatingTime("09:00 ~ 12:00")
+                .loanAvailability("가능")
+                .createdAt(currentTime)
+                .updatedAt(currentTime)
+                .lendingAvailableCount(4)
+                .lendingAvailableDays(3)
+                .overdueCount(2)
+                .lendingLimitDays(5)
                 .build();
         //when
         LibraryEntity result2 = memoryEntityRepository.create(command2);
