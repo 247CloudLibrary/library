c6ed82a8f20b86be461d74871bb1244157d41985
yongCloude <djkye0001@naver.com>
2022-04-04 13:35:52 +0900

    [FEAT] 도서관 등록 관련 기능 인터페이스 추가

--- src/main/java/com/cloudlibrary/library/infrastructure/persistance/Memory/repository/LibraryEntityRepository.java
+++ src/main/java/com/cloudlibrary/library/infrastructure/persistance/Memory/repository/LibraryEntityRepository.java
@@ -1,14 +1,16 @@
 package com.cloudlibrary.library.infrastructure.persistance.Memory.repository;
 
 import com.cloudlibrary.library.application.domain.Library;
+import com.cloudlibrary.library.application.service.LibraryOperationUseCase;
 import com.cloudlibrary.library.application.service.LibraryReadUseCase;
+import com.cloudlibrary.library.infrastructure.persistance.Memory.Entity.LibraryEntity;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface LibraryEntityRepository {
 
-    LibraryReadUseCase.FindLibraryResult create(Library library);
+    LibraryEntity create(LibraryOperationUseCase.LibraryCreatedCommand command);
     Optional<Library> findLIbraryById(long libraryId);
     List<Library> findLibraryAll();
 
