1ea366c450e71e0f40e49a045e89aad4e280d8af
yongCloude <djkye0001@naver.com>
2022-04-04 15:26:40 +0900

    [FEAT] 도서관 등록(POST) 관련 request body 추가

---
+++ src/main/java/com/cloudlibrary/library/ui/requestBody/LibraryCreateRequest.java
@@ -0,0 +1,18 @@
+package com.cloudlibrary.library.ui.requestBody;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter
+@ToString
+@NoArgsConstructor
+public class LibraryCreateRequest {
+    private long id;
+    private String name;
+    private String address;
+    private String email;
+    private String tel;
+    private String holiday;
+}
