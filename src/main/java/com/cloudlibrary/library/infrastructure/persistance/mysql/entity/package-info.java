fd64680619ac4f8dd2db3d4e373818eb86cb5367
yongCloude <djkye0001@naver.com>
2022-04-05 22:09:57 +0900

    [create] package-info 추가

---
+++ src/main/java/com/cloudlibrary/library/infrastructure/persistance/mysql/entity/package-info.java
@@ -0,0 +1,4 @@
+package com.cloudlibrary.library.infrastructure.persistance.mysql.entity;
+
+
+// TODO : JAP Entity 추가
---
+++ src/main/java/com/cloudlibrary/library/infrastructure/persistance/mysql/repository/package-info.java
@@ -0,0 +1,4 @@
+package com.cloudlibrary.library.infrastructure.persistance.mysql.repository;
+
+
+//TODO: JPA Repository 추가
