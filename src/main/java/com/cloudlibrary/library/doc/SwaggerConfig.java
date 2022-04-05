552b5e7c57cc22268d3f09c8c10967f56c8c0b1c
yongCloude <djkye0001@naver.com>
2022-04-05 22:10:24 +0900

    [doc] 패키지 위치 수정

--- src/main/java/com/cloudlibrary/library/doc/SwaggerConfig.java
+++ src/main/java/com/cloudlibrary/library/doc/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.cloudlibrary.doc;
+package com.cloudlibrary.library.doc;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
