b17d3968e99a652c0974d9b6888d1e26215ff888
yongCloude <djkye0001@naver.com>
2022-04-12 23:15:24 +0900

    [doc] @EnableFeignClients 어노테이션 추가

--- src/main/java/com/cloudlibrary/library/LibraryApplication.java
+++ src/main/java/com/cloudlibrary/library/LibraryApplication.java
@@ -2,8 +2,10 @@ package com.cloudlibrary.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableFeignClients
 @EnableWebMvc
 @SpringBootApplication
 public class LibraryApplication {
