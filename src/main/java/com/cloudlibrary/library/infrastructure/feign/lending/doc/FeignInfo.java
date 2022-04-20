cde718e93c57bacf3497aa0f6ef3135c3f5f3266
yongCloude <djkye0001@naver.com>
2022-04-20 15:27:43 +0900

    [doc] Feign 통신에서 기입해야 하는 문자열 관리 클래스

---
+++ src/main/java/com/cloudlibrary/library/infrastructure/feign/lending/doc/FeignInfo.java
@@ -0,0 +1,8 @@
+package com.cloudlibrary.library.infrastructure.feign.lending.doc;
+
+public class FeignInfo {
+
+    public static final String VALUE = "lending-service";
+    public static final String URI = "ecs-alb-167470959.us-east-1.elb.amazonaws.com/v1";
+    public static final String PATCH_LIBRARY_RULES_URI = "/lending/libraries-rules";
+}
