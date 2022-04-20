b342c7abf177f16be788859b240770320c77f193
yongCloude <djkye0001@naver.com>
2022-04-20 11:14:13 +0900

    [update] todo 제거

--- src/main/java/com/cloudlibrary/library/infrastructure/feign/lending/service/FeignLendingService.java
+++ src/main/java/com/cloudlibrary/library/infrastructure/feign/lending/service/FeignLendingService.java
@@ -12,7 +12,7 @@ public class FeignLendingService {
 
         LibraryRulesUpdateResponse updatedRules = lendingFeignClient.createRules(request);
 
-        // TODO: 2022/04/12 예외처리 상황 생각해 보기
+
         return updatedRules;
     }
 
